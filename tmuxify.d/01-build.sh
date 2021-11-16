c2f82da575c0fdafd4c9819e4ad42efd391afb09
Aaron Faanes <dafrito@gmail.com>
2021-11-16 00:23:41 -0600

    Update microproject

---
+++ tmuxify.d/01-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true; do
+    npm run build
+    make build SITE_URL=$SITE_URL SITE_PORT=$SITE_PORT &
+    serverpid=$!
+    inotifywait -e modify -r package*.json tsconfig.json .babelrc webpack* src
+    kill -TERM $serverpid
+done
