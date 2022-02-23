7f8ee9c8ec6a9a9fe60b1982be597fe0511cdd83
Aaron Faanes <dafrito@gmail.com>
2022-02-23 01:57:41 -0600

    Update Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,9 @@
-const path = require("path");
+const {webpackConfig, relDir} = require("./webpack.common");
 
 module.exports = {
-  externals: {
-    "parsegraph-checkglerror":{
-      commonjs:"parsegraph-checkglerror",
-      commonjs2:"parsegraph-checkglerror",
-      amd:"parsegraph-checkglerror",
-      root:"parsegraph_checkglerror"
-    }
+  entry: {
+    index: relDir("src/index.ts"),
+    demo: relDir("src/demo.ts"),
   },
-  entry: path.resolve(__dirname, "src/shader.ts"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "parsegraph-shader.js",
-    globalObject: "this",
-    library: "parsegraph_shader",
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx?)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'ts-loader']
-      },
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ["ts-shader-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".ts", ".tsx", ".glsl"],
-    modules: [
-      path.resolve(__dirname, "src"),
-      path.resolve(__dirname, "node_modules"),
-    ]
-  },
-  mode: "development",
-  devtool: "eval-source-map",
+  ...webpackConfig(false),
 };
